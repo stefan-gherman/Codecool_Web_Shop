3249db0bc7fb3da1cb689eb7d15793052d889760
Stefan Gherman <scgheman@gmail.com>
2020-04-28 13:05:36 +0300

    Add CartDao interface

---
+++ src/main/java/com/codecool/shop/dao/CartDao.java
@@ -0,0 +1,15 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.model.Product;
+
+import java.util.List;
+
+public interface CartDao {
+
+    void add(Product product);
+    void remove(Product product);
+    List<Product> getCartContents();
+
+
+
+}
