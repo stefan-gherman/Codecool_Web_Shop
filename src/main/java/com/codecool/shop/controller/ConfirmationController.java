fb9db201850ef7c6a4d1cab0326229a5561b7139
dandumitriu33 <dandumitriu33@gmail.com>
2020-04-29 23:01:56 +0300

    Add confirmation email sending - complete

--- src/main/java/com/codecool/shop/controller/ConfirmationController.java
+++ src/main/java/com/codecool/shop/controller/ConfirmationController.java
@@ -57,9 +57,9 @@ public class ConfirmationController extends HttpServlet {
 
         Properties props = new Properties();
         props.put("mail.smtp.host", host);
-//        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.port", "587");
         props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.enable", "true");
 
         Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
@@ -81,7 +81,8 @@ public class ConfirmationController extends HttpServlet {
 
             System.out.println("message sent successfully...");
 
-        } catch (MessagingException e) {e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
         }
     }
 
