b8c516dc3b8aa219d30fce9aa094243279660bde
Stefan Gherman <scgheman@gmail.com>
2020-05-26 09:47:51 +0300

    Add test Class

---
+++ src/test/java/com/codecool/shop/dao/implementation/UserDaoJDBCTest.java
@@ -0,0 +1,20 @@
+package com.codecool.shop.dao.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoJDBCTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void getUserByEmail() {
+    }
+
+    @Test
+    void getUserPasswordByEmail() {
+    }
+}
