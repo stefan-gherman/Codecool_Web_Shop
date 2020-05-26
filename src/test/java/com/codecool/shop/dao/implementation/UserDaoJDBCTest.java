abf5045221135933dc9e9de31c27b83286985579
Stefan Gherman <scgheman@gmail.com>
2020-05-26 12:31:44 +0300

    Create test DBConnectTest class

--- src/test/java/com/codecool/shop/dao/implementation/UserDaoJDBCTest.java
+++ src/test/java/com/codecool/shop/dao/implementation/UserDaoJDBCTest.java
@@ -1,10 +1,16 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.connection.DBConnect;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserDaoJDBCTest {
+    DBConnect
+    @BeforeEach
+    public void
 
     @Test
     void add() {
